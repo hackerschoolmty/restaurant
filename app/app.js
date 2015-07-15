ccdd94fcb10100fa3aff77db5324f4f4e40f2048
bhserna <bhserna@gmail.com>
2015-07-14 20:05:01 -0500

    pretend to test the app manually

--- app/app.js
+++ app/app.js
@@ -2,6 +2,26 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import Pretender from "pretender";
+
+new Pretender(function(){
+  this.get("/items", function() {
+    var items = {
+      items: [
+        { id: 1, name: "Tradicional", price: "50", image_url: "/images/burger-1.jpeg"},
+        { id: 2, name: "Funky bread", price: "70", image_url: "/images/burger-2.jpeg"},
+        { id: 3, name: "Con queso", price: "60", image_url: "/images/burger-3.jpeg"},
+        { id: 4, name: "Con jamón", price: "80", image_url: "/images/burger-4.jpeg"},
+        { id: 5, name: "Especial", price: "50", image_url: "/images/burger-1.jpeg"},
+        { id: 6, name: "La nice", price: "70", image_url: "/images/burger-2.jpeg"},
+        { id: 7, name: "La del chavo", price: "60", image_url: "/images/burger-3.jpeg"},
+        { id: 8, name: "La del barrio", price: "80", image_url: "/images/burger-4.jpeg"}
+      ]
+    };
+
+    return [200, {"Content-Type": "application/json"}, JSON.stringify(items)];
+  });
+});
 
 var App;
 
